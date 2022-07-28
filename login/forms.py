19b9897fd0602fa12a1eefc48523442e2a5aa80c
Konee <104918313+KoneeVibes@users.noreply.github.com>
2022-07-27 21:57:40 -0500

    tutorial

--- login/forms.py
+++ login/forms.py
@@ -1,8 +1,11 @@
-from django.forms import ModelForm
+# from django.forms import ModelForm
+from django import forms
 from django.contrib.auth.models import User
 from django.utils.translation import gettext as _
 
-class logindetails(ModelForm):
+class logindetails(forms.ModelForm):
+    username = forms.CharField(label=_('name'))
+    password = 
     class Meta:
         model = User
         fields = ['username', 'password']
--- login/views.py
+++ login/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
 from .forms import logindetails
 from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import User
 
 
 def signin(request):
@@ -10,12 +11,15 @@ def signin(request):
         form = logindetails(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username') 
-            password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=password)
+            user = form.objects.create_user(
+                username = form.cleaned_data['username'], 
+                password = form.cleaned_data['password']
+            )  
+            login(request, user) 
+            authenticate(username=request.GET['username'], password=request.GET['password']) 
             if user is not None:
                 login(request, user)
-            return redirect('home')
+            return redirect('home')           
     else:
         form = logindetails()
     return render(request, 'login/login.html', {'form': form})
