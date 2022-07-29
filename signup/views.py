5c946eadeed22582dd1e24c6a0fcafd7b7321581
Konee <104918313+KoneeVibes@users.noreply.github.com>
2022-07-28 20:50:43 -0500

    final edits on the signup app

--- signup/views.py
+++ signup/views.py
@@ -1,23 +1,22 @@
 from django.shortcuts import render , redirect
 from django.contrib.auth import login, authenticate
-from .forms import SignUpForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 
 def signup(request):
     if request.user.is_authenticated:
         return redirect('home')
-    if request.method == "POST":
-        form = SignUpForm(request.POST) 
-
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            username = request.POST['username'] 
-            password = request.POST['password']
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
             user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect('home')
+            login(request, user)
+            messages.success(request, ('Registration Succesful'))
+            return redirect('home')
     else:
-        form = SignUpForm(initial={'email':'@gmail.com', 'username':request.user.username}) 
+        form = UserCreationForm()
     return render(request, 'signup/index.html', {'form': form})
-
